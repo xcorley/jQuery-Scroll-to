692ac327b1a4d40da29c092b82edb359088d1c89
Chad Corley <xcorley@yahoo.com>
2014-02-26 14:22:51 -0600

    Update jQscrollTo.js

--- jQscrollTo.js
+++ jQscrollTo.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 		$(".clicker").click(function(){
 			$(this).toggleClass("active");
-			$(this).next(".wpcf7").stop('true','true').slideToggle("slow");
+			$(this).next(".target").stop('true','true').slideToggle("slow");
 		});
 		$(".menu-contact").click(function conscroll(){
 			$(".clicker").toggleClass("active");
-			$(".wpcf7").stop('true','true').slideToggle(1000);
+			$(".target").stop('true','true').slideToggle(1000);
       var scrollwait=setInterval(function(){conscroll()},2001);
-      if ($(".wpcf7").is(':visible')) {
+      if ($(".target").is(':visible')) {
      $("html, body").animate({scrollTop: $("#contact").offset().top});
       clearInterval(scrollwait);
     }
